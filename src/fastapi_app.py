e18679a2b039106a1f7ef67ebe20a346407df92a
MIK-RC <muhammad.ibrahimkhan@royalcyber.com>
2026-01-09 19:56:20 +0500

    Fix CORS issue

--- src/fastapi_app.py
+++ src/fastapi_app.py
@@ -10,21 +10,26 @@ import boto3
 import uvicorn
 from dotenv import load_dotenv
 from fastapi import FastAPI, Request, Response
+from fastapi.middleware.cors import CORSMiddleware
 
 # Load .env credentials
 load_dotenv()
 
-CORS_HEADERS = {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Headers": "Content-Type,Authorization",
-    "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
-}
+# Setting env variables
 PORT = 8000
 AWS_REGION = os.environ.get("AWS_DEFAULT_REGION")
 AGENTCORE_AGENT_RUNTIME_ARN = os.environ.get(
     "AGENTCORE_AGENT_RUNTIME_ARN"
 )  # Get from AgentCore UI on AWS.
 app = FastAPI()
+
+# Setting up CORS Middelware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["POST", "OPTIONS"],
+    allow_headers=["Content-Type", "Authorization"],
+)
 
 
 @app.post("/invoke")
